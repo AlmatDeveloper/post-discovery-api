aa814802a8dabe1a819f6ff8887091e3c7fb50a2
Almat Melis <almatmelis8@gmail.com>
2021-09-15 23:14:19 +0600

    qwe

--- src/main/java/kz/dar/intership/summer/PostDiscoveryApiApplication.java
+++ src/main/java/kz/dar/intership/summer/PostDiscoveryApiApplication.java
@@ -2,7 +2,9 @@ package kz.dar.intership.summer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class PostDiscoveryApiApplication {
 
